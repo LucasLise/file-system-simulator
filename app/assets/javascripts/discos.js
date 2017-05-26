36c31941ee6de165f42b2f1d9c470d5d3665a1fb
Lucas Lise <lucas_lise1@hotmail.com>
2017-05-26 17:47:00 -0300

    instruções de ajuda

---
+++ app/assets/javascripts/discos.js
@@ -0,0 +1,17 @@
+$(document).on("turbolinks:load", function(){
+  var button_informacoes = $("#button-informacoes")
+  var informacoes = $("#informacoes")
+
+  button_informacoes.click(function(){
+     if(informacoes.is(':visible')){
+       button_informacoes.find("i").removeClass('glyphicon glyphicon-chevron-up').addClass('glyphicon glyphicon-chevron-down');
+     }else{
+       button_informacoes.find("i").removeClass('glyphicon glyphicon-chevron-down').addClass('glyphicon glyphicon-chevron-up');
+     }
+     informacoes.slideToggle('slow');
+  });
+
+  $(".alert").fadeTo(2000, 500).slideUp(1000, function(){
+      $(".alert").slideUp(1000);
+  });
+});
